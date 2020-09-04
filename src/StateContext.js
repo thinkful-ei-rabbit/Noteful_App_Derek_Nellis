53841983691a2fb08925c2892f48717ee97b77ae
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-09-04 12:09:13 -0700

    SAVEPOINT

--- src/StateContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const StateContext = React.createContext({
-  notes: [],
-  folders: []
-})
-
-export default StateContext
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
+  API_KEY: process.env.REACT_APP_API_KEY,
+}
